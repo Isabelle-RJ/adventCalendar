7974620d60e48de7c210a7b4048d0f4a21afcd1e
Isabelle <isabelle_radermecker@icloud.com>
2024-12-29 17:58:08 +0100

    LoginController and LoginFormRequest

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json('Jade');
+    return response()->json('ce que tu veux');
 });
 
 Route::post('/register', RegisterController::class);
-
+Route::get('/login', LoginController::class);
